5a0b84ac676ddf052b8f8adcaf920fb087908704
Bryan Lee <bryan@brlee.com>
2023-10-04 14:54:33 -0400

    refactor(fileupload): added react-hook-form to FileUpload component

--- src/app/projects/create/client_page.tsx
+++ src/app/projects/create/client_page.tsx
@@ -65,7 +65,7 @@ export const CreateProject: React.FC = () => {
         <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
           Create a new Project
         </h2>
-        <form action="#">
+        <form onSubmit={handleSubmit(handleCreateProject)}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
               <label
@@ -77,11 +77,10 @@ export const CreateProject: React.FC = () => {
               <div className="w-full">
                 <input
                   type="text"
-                  name="name"
                   id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Type project name"
-                  required
+                  {...register('name', { required: true })}
                 />
               </div>
             </div>
@@ -94,11 +93,10 @@ export const CreateProject: React.FC = () => {
               </label>
               <input
                 type="text"
-                name="brand"
                 id="brand"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Product brand"
-                required
+                {...register('brand')}
               />
             </div>
             <div className="w-full">
@@ -110,11 +108,10 @@ export const CreateProject: React.FC = () => {
               </label>
               <input
                 type="number"
-                name="price"
                 id="price"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="$2999"
-                required
+                {...register('price')}
               />
             </div>
             <div>
@@ -126,6 +123,7 @@ export const CreateProject: React.FC = () => {
               </label>
               <select
                 id="category"
+                {...register('category')}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               >
                 <option value="" disabled selected>
@@ -146,11 +144,10 @@ export const CreateProject: React.FC = () => {
               </label>
               <input
                 type="number"
-                name="item-weight"
                 id="item-weight"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="12"
-                required
+                {...register('itemWeight')}
               />
             </div>
 
@@ -166,6 +163,7 @@ export const CreateProject: React.FC = () => {
                 rows={8}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Your description here"
+                {...(register('description'), { required: true })}
               ></textarea>
             </div>
             <FileUpload />
--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -1,17 +1,27 @@
-'use client';
-
 import { FileInput, Label } from 'flowbite-react';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function FileUpload() {
+  const { control } = useForm();
+
   return (
     <div className="max-w-md" id="fileUpload">
       <div className="mb-2 block">
         <Label htmlFor="file" value="Upload files" />
       </div>
-      <FileInput
-        helperText="Add multiple files for your project"
-        id="file"
-        multiple
+      <Controller
+        name="file"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <FileInput
+            {...field}
+            helperText="Add multiple files for your project"
+            id="file"
+            multiple
+          />
+        )}
       />
     </div>
   );
