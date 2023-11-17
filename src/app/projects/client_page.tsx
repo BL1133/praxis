2828ad65271f7289bcb12640d216a4f7ed8f313d
Bryan Lee <bryan@brlee.com>
2023-11-17 00:08:24 -0500

    fix(globals.css): fix colors

--- src/app/projects/client_page.tsx
+++ src/app/projects/client_page.tsx
@@ -16,9 +16,12 @@ export const Projects: React.FC<{ projects: GetProjectsResponse }> = ({
   if (isLoading) return <div>Loading projects...</div>;
 
   return (
-    <div>
+    <div className="flex flex-col place-items-center gap-5 mt-10">
       {data.docs.map((project: Project) => (
-        <div key={project.id}>
+        <div
+          key={project.id}
+          className="w-10/12 bg-white dark:bg-gray-800  shadow-elevation-medium rounded-lg"
+        >
           <h1>{project.title}</h1>
           <p>{project.shortDescription}</p>{' '}
           {/* Changed this to a <p> tag for semantic correctness */}
