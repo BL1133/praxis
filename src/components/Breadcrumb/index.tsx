e27ee04fefa510bc399869c975a4ac7effa4b633
Bryan Lee <bryan@brlee.com>
2023-12-06 17:41:50 -0500

    fix(navbar): modified link in nav for projects page

---
+++ src/components/Breadcrumb/index.tsx
@@ -0,0 +1,14 @@
+import { Breadcrumb } from 'flowbite-react';
+import { HiHome } from 'react-icons/hi';
+
+export function BreadcrumbComponent() {
+  return (
+    <Breadcrumb aria-label="Default breadcrumb example">
+      <Breadcrumb.Item href="#" icon={HiHome}>
+        Home
+      </Breadcrumb.Item>
+      <Breadcrumb.Item href="#">Projects</Breadcrumb.Item>
+      <Breadcrumb.Item>Flowbite React</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -76,6 +76,14 @@ const Nav: FC = function () {
                 Flowbite
               </span>
             </Navbar.Brand>
+            <Link
+              href={'/projects'}
+              className="ml-8 text-gray-500 flex gap-2 p-2 rounded-lg hover:text-gray-900 dark:text-gray-400 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              <p className="text-md font-semibold mt-0.5 text-cyan-500">
+                Projects
+              </p>
+            </Link>
           </div>
           {!data?.user && (
             <div>
